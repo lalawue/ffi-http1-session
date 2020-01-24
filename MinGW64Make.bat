0436f669f3035a7afa48911b6a04b4fe989fc680
lalawue <suchaaa@gmail.com>
2020-01-25 00:21:11 +0800

    rename lib under windows

--- MinGW64Make.bat
+++ MinGW64Make.bat
@@ -1,2 +1,2 @@
 mkdir build
-gcc -O2 -Isrc src\http_parser.c src\pull_style_api.c -shared -fPIC -o build\libhyperparser.dll
+gcc -O2 -Isrc src\http_parser.c src\pull_style_api.c -shared -fPIC -o build\hyperparser.dll
